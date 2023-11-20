3900c206e8d085551cf7442e5474248c5356299c
utensil <utensil@users.noreply.github.com>
2023-11-20 03:34:21 +0000

    deploy: f3dbaedaba580ae9e0fd19d1aa170f7751a9ae7b

--- blueprint/js/script.js
+++ blueprint/js/script.js
@@ -5,16 +5,21 @@ $(document).ready(function() {
     $("nav.toc").off("click", "span.expand-toc");
 
     $("nav.toc").on("click", "span.expand-toc",
-    function() {
-        $(this).siblings("ul").slideToggle('fast');
+        function() {
+            $(this).siblings("ul").slideToggle('fast');
 
-        if ($(this).html() == "▼") {
-            $(this).html("▶");
-            $(this).addClass("collapse");
-        } else {
-            $(this).html("▼");
-            $(this).removeClass("collapse");
-        };
+            if ($(this).html() == "▼") {
+                $(this).html("▶");
+                $(this).addClass("collapse");
+            } else {
+                $(this).html("▼");
+                $(this).removeClass("collapse");
+            };
+        });
 
-    })
+    $('a.lean_decl').on('click', function(e){ 
+        e.preventDefault(); 
+        var url = $(this).attr('href'); 
+        window.open(url, '_blank');
+    });
 });
