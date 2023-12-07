bae0ad1e622f711533f463de0a42c731b1510bdf
utensil <utensilcandel@gmail.com>
2023-12-08 01:13:50 +0800

    Fix dep graph

--- tasks.py
+++ tasks.py
@@ -38,10 +38,10 @@ def bp(ctx):
 
     cwd = os.getcwd()
     os.chdir(BP_DIR)
-    run('mkdir -p print && cd src && xelatex -output-directory=../print print.tex')
+    run('mkdir -p print && cd src && xelatex --shell-escape -output-directory=../print print.tex')
     run('cd print && bibtex print.aux', env={'BIBINPUTS': '../src'})
-    run('cd src && xelatex -output-directory=../print print.tex')
-    run('cd src && xelatex -output-directory=../print print.tex')
+    run('cd src && xelatex --shell-escape -output-directory=../print print.tex')
+    run('cd src && xelatex --shell-escape -output-directory=../print print.tex')
     run('cp print/print.bbl src/web.bbl')
     os.chdir(cwd)
 
